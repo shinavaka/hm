49dbca77ab901fcf9a2ca179e665e5255d1394df
shinavaka <cbohnstengel@gmail.com>
2025-01-15 19:40:59 -0600

    Create elemental-spells.lua

---
+++ elemental-spells.lua
@@ -0,0 +1,11 @@
+local Unlocker, awful, project = ...
+local ele = project.shaman.ele
+
+local Spell = awful.Spell
+awful.Populate({
+    moonfire = Spell(196840),
+}, ele, getfenv(1))
+
+frostshock:Callback(function(spell)
+    spell:Cast(target)
+end)

e2511acef3ce54381278734ec265cac6f3fff4f8
shinavaka <cbohnstengel@gmail.com>
2025-01-15 19:43:11 -0600

    Update core.lua

--- core.lua
+++ core.lua
@@ -4,3 +4,5 @@ awful.DevMode = true
 
 project.druid = {}
 project.druid.guardian = awful.Actor:New({ spec = 3, class = "druid" })
+project.shaman = {}
+project.shaman.ele = awful.Actor:New({ spec = 1, class = "shaman" })
